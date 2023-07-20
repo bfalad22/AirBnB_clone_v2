2d8e47181550afd9e06affb56a89c254b5c604a4
bfalad22 <bolasaliu93@gmail.com>
2023-07-20 08:54:21 -0700

    AirBnB_clone v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-echo 'Hello there!!!' > /data/web_static/releases/test/index.html
+echo 'Holberton School' > /data/web_static/releases/test/index.html
 sudo chown -R ubuntu:ubuntu /data/
 printf %s "server {
     listen 80 default_server;

f58a683e3144e12a2456911a5c3556023d5911e6
bfalad22 <bolasaliu93@gmail.com>
2023-07-20 06:31:18 -0700

    AirBnB Clone V2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,34 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#set up your web servers for the deployment of web_static
 
-apt-get update
+apt-get update -y
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
+echo 'Hello there!!!' > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
+    index  index.html index.htm index.nginx-debian.html;
+    server_name _;
+    
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+	index index.html index.htm;
+	autoindex off;
     }
-
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://bfalad22.tech;
     }
-
     error_page 404 /404.html;
     location /404 {
-      root /var/www/html;
+      root /var/www/error;
       internal;
     }
 }" > /etc/nginx/sites-available/default
-
 service nginx restart
